6641c4d54bfc4ac54b664b58abed90b5e7723785
Bladestar2105 <55372949+Bladestar2105@users.noreply.github.com>
2026-03-17 13:39:11 +0000

    Fix headers incorrectly applying to non-proxy handlers

--- src/caddy.js
+++ src/caddy.js
@@ -506,24 +506,6 @@ function generateCaddyfile(config, certsDir = './certs') {
             }
           }
 
-          // Handler Headers (pre-proxy setup)
-          if (handler.header) {
-            for (const hID of handler.header) {
-              const h = headersMap[hID];
-              if (h) {
-                const dir = h.headerUpDown || 'header_up';
-                if (directive !== 'reverse_proxy') {
-                  const action = h.headerValue ? 'set' : '-';
-                  if (action === '-') {
-                    sb += `\t\theader -${h.headerType}\n`;
-                  } else {
-                    sb += `\t\theader ${h.headerType} ${h.headerValue}\n`;
-                  }
-                }
-              }
-            }
-          }
-
           // Upstreams
           if (directive === 'reverse_proxy') {
             sb += '\t\treverse_proxy';
